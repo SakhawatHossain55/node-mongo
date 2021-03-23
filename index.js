2e5ca5c6c17124f9174d6da41a3914cf0442d8ef
SakhawatHossain55 <sakhawatHossain7969@gmail.com>
2021-03-23 11:51:30 +0600

    added text

--- index.js
+++ index.js
@@ -3,9 +3,26 @@ const express = require('express')
 
 const app = express();
 
+const users = ['Asad', 'Moin', 'Sabed', 'Oishee', 'Jannat', 'sumaya']
+
 
 app.get('/', (req, res) =>{
-    res.send('I know how to open Node...YAY')
+    const fruit = {
+        product: 'ada',
+        price: 220
+    }
+    res.send(fruit)
 })
+app.get('/fruits/banana', (req, res) =>{
+    res.send({fruit:'banana', quantity: 1000, price:1000})
+})
+
+app.get('/users/:id',(req,res) =>{
+    const id = req.params.id;
+    console.log(req.query.sort);
+    const name = users[id];
+    res.send({id, name});
+})
+
 
 app.listen(3000, () => console.log('Listenting to portt 3000'));
